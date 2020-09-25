a99eb57e66be05d93870d6ab8fac115ee66706fe
Ignacio Garcia <nachog.2000@hotmail.com>
2020-09-24 23:39:37 -0300

    Tratando de ver la suma. Creo q fin por hoy :)

--- src/utils.c
+++ src/utils.c
@@ -22,7 +22,7 @@ NumeroAstronomico *realizarSuma() {
     num2 = crearNumero();
 
     printf("Ahora suma:\n");
-    NumeroAstronomico *result = sumar(num1, num2);
+    NumeroAstronomico *result = (num1, num2);
     mostrar(result, 3);
     system("pause");
     return result;
@@ -36,7 +36,7 @@ void verificarIgualdad() {
     printf("Segundo valor:\n");
     num2 = crearNumero();
 
-    sonIguales(num1,num2); //TODO: Armar mensaje
+    sonIguales(num1,num2);
 }
 
 void obtenerMenor() {
