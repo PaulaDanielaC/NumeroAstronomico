ff3a489101b925b4e2cc9fad2fe68a58bffeef34
Ignacio Garcia <nachog.2000@hotmail.com>
2020-10-02 17:51:52 -0300

    Rediseño

--- src/aritm_op.c
+++ src/aritm_op.c
@@ -14,16 +14,17 @@ NumeroAstronomico *sumar(NumeroAstronomico *num1, NumeroAstronomico *num2) {
     NumeroAstronomico *resultado = malloc(sizeof(NumeroAstronomico));
     int numMasLargo = obtenerNumMasDigitos(num1->longitudError, num2->longitudError);
     resultado->entero = malloc(numMasLargo);
-    int index = numMasLargo-1;
-    int long1 = num1->longitudError-1;
-    int long2 = num2->longitudError-1;
+    int index = numMasLargo - 1;
+    int long1 = num1->longitudError - 1;
+    int long2 = num2->longitudError - 1;
     int carry = 0;
     int digitoResultado;
     int overflow = 0;
+    int i = 0;
 
-    while(long1 != -1 && long2 != -1) {
-        int d1 = ((int)num1->entero[long1]) - ((int)'0');
-        int d2 = ((int)num2->entero[long2]) - ((int)'0');
+    while (long1 != -1 && long2 != -1) {
+        int d1 = ((int) num1->entero[long1]) - ((int) '0');
+        int d2 = ((int) num2->entero[long2]) - ((int) '0');
         digitoResultado = d1 + d2 + carry;
 
         if (digitoResultado > 9) {
@@ -33,19 +34,15 @@ NumeroAstronomico *sumar(NumeroAstronomico *num1, NumeroAstronomico *num2) {
             carry = 0;
         }
 
-        resultado->entero[index] = (char)(digitoResultado + '0');
+        resultado->entero[index] = (char) (digitoResultado + '0');
         long1--;
         long2--;
         index--;
     }
 
 //   TODO: Terminar de completar el numero si hay diferencia de digitos
-//   TODO: Manejo del caso overflow  ----> No me acuerdo si iría 103 (ya con el carry y el overflow incluidos) o 101
+//   TODO: Manejo del caso overflow
 
-
-    resultado->entero[numMasLargo] = '\0';
-    printf("resultado: %s\n", resultado->entero);
-    return resultado;
 }
 
 int obtenerNumMasDigitos(int longNum1, int longNum2) {
@@ -75,12 +72,11 @@ int sonIguales(NumeroAstronomico *num1, NumeroAstronomico *num2) {
 int esMenor(NumeroAstronomico *num1, NumeroAstronomico *num2) {
     if (getTipoDeError(num1) == Ninguno || getTipoDeError(num2) == Ninguno) { // TODO: Idem de sonIguales
         if (strcmp(num1->entero, num2->entero) < 0) {
-            printf("Es menor %s\n\n\n", num1->entero); // TODO: Implementar primero el numero y despues el texto.
+            printf("\n%s es menor \n\n\n", num1->entero);
             system("pause");
             return 1;
-        }
-        else{
-            printf("Es menor %s\n", num2->entero);
+        } else {
+            printf("\n%s es menor \n\n\n", num2->entero);
             system("pause");
             return 0;
         }
