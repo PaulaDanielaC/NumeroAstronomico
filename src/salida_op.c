32edfc763c6a0483a9c5e718414ec5d072aa9fc3
Paula <pau.chittaro@outlook.com>
2020-10-24 16:12:48 -0300

    Nos quedan los puntos bien impresos

--- src/salida_op.c
+++ src/salida_op.c
@@ -1,11 +1,20 @@
 #include <NumeroAstronomico.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <mem.h>
 
-char* darVuelta(char*, int);
+void ponerPuntos(NumeroAstronomico* num, char* numero);
+void ponerEspacios(char* numero, unsigned int cantGrupos);
 
 void mostrar(NumeroAstronomico* num, unsigned int cantGrupos, FILE* stream) {
 
+    char* cadena = malloc(sizeof(char));
+    ponerPuntos(num, cadena);
+    ponerEspacios(cadena, cantGrupos);
+    fprintf(stream, "%s\n", cadena);
+}
+
+void ponerPuntos(NumeroAstronomico* num, char* numero) {
     int cantPuntos;
     if((num->longitudError % 3) != 0)
         cantPuntos = num->longitudError / 3;
@@ -15,32 +24,25 @@ void mostrar(NumeroAstronomico* num, unsigned int cantGrupos, FILE* stream) {
     int cantCaracteres = (num->longitudError - 1) + cantPuntos;
     int cantLeidos = num->longitudError;
     int cantImpresos = 0;
-    char* cadena = malloc(sizeof(char ));
 
     while (cantCaracteres >= 0) {
+
         if(cantImpresos != 0 && cantImpresos != num->longitudError && cantImpresos % 3 == 0) {
-            cadena[cantCaracteres] = '.';
+            numero[cantCaracteres] = '.';
             cantCaracteres--;
         }
 
-        cadena[cantCaracteres] = num->entero[cantLeidos-1];
+        numero[cantCaracteres] = num->entero[cantLeidos-1];
         cantLeidos--;
         cantCaracteres--;
         cantImpresos++;
     }
-
-    fprintf(stream, "%s", cadena);
 }
 
-char* darVuelta(char* numero, int longitud) {
-    char* resultado = malloc(longitud);
-    int cantMax = longitud;
+void ponerEspacios(char* numero, unsigned int cantGrupos) {
 
-    for (int i = 0; i < longitud; i++) {
-        resultado[i] = numero[cantMax-1];
-        cantMax--;
-    }
+    int longitud = (int) strlen(numero);
+    char* cadenaAux;
 
-    resultado[longitud] = '\0';
-    return resultado;
+
 }
